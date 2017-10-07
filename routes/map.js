f4ee59ca572cef0f703eab5169527d62e2268f8f
Jonathan Jensen Jr <jonathanjensenjr@gmail.com>
2017-10-07 15:14:52 -0700

    map routes, use and render running

--- routes/map.js
+++ routes/map.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('map', { title: 'Express' });
+router.get('/map', function(req, res, next) {
+  res.render('map', { title: 'Skate-Spotter' });
 });
 
 module.exports = router;
