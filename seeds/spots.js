368705fbe96714966f707ccf2c2954a3e77adb94
Jonathan Jensen Jr <jonathanjensenjr@gmail.com>
2017-10-09 16:02:59 -0700

    more spots & for loop added

--- seeds/spots.js
+++ seeds/spots.js
@@ -17,44 +17,51 @@ exports.seed = function(knex, Promise) {
         photo_url: '',
         description: 'Small quarter pipes, hips and flatbars'
 
-      },
-      //{
-      //   id: 2,
-      //   name: 'Boulder Public Library',
-      //   location: '1001 Arapahoe Ave, Boulder, CO 80302',
-      //   bust: 'medium',
-      //   difficulty: 'hard',
-      //   photo_url: '',
-      //   description: 'tall benches across flat gaps'
-      //
-      // },{
-      //   id: 3,
-      //   name: 'The Tea House',
-      //   location: '1770 13th St, Boulder, CO 80302',
-      //   bust: 'low',
-      //   difficulty: 'easy',
-      //   photo_url: '',
-      //   description: 'large flat ground area, small drop with wallride, ledges and six stair with wall-rail'
-      //
-      // },{
-      //   id: 4,
-      //   name: 'Sacred Heart',
-      //   location: '1318 Mapleton Ave, Boulder, CO 80304',
-      //   bust: 'high',
-      //   difficulty: 'high',
-      //   photo_url: '',
-      //   description: 'large and medium sized gaps, 10 stair over the rail gap'
-      //
-      // },{
-      //   id: 5,
-      //   name: 'The Hill Ledges',
-      //   location: '1111 Broadway, Boulder, CO 80302',
-      //   bust: 'low',
-      //   difficulty: 'low-medium',
-      //   photo_url: '',
-      //   description: 'ledges flowing downhill'
-      //
-      // }
+      },{
+        id: 2,
+        lat: 40.014938,
+        lon: -105.281812,
+        name: 'Boulder Public Library',
+        location: '1001 Arapahoe Ave, Boulder, CO 80302',
+        bust: 'medium',
+        difficulty: 'hard',
+        photo_url: '',
+        description: 'tall benches across flat gaps'
+
+      },{
+        id: 3,
+        lat: 40.016384,
+        lon: -105.277263,
+        name: 'The Tea House',
+        location: '1770 13th St, Boulder, CO 80302',
+        bust: 'low',
+        difficulty: 'easy',
+        photo_url: '',
+        description: 'large flat ground area, small drop with wallride, ledges and six stair with wall-rail'
+
+      },{
+        id: 4,
+        lat: 40.021511,
+        lon: -105.278712,
+        name: 'Sacred Heart',
+        location: '1318 Mapleton Ave, Boulder, CO 80304',
+        bust: 'high',
+        difficulty: 'high',
+        photo_url: '',
+        description: 'large and medium sized gaps, 10 stair over the rail gap'
+
+      },{
+        id: 5,
+        lat: 40.007552,
+        lon: -105.275076,
+        name: 'The Hill Ledges',
+        location: '1111 Broadway, Boulder, CO 80302',
+        bust: 'low',
+        difficulty: 'low-medium',
+        photo_url: '',
+        description: 'ledges flowing downhill'
+
+      }
     ])
     .then(()=>{
       return knex.raw("SELECT setval('spots_id_seq', (SELECT MAX(id) FROM spots))")
